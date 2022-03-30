3607f910bfbac4b02f81cb121171cedb292bdbed
prathame <42273190+prathame@users.noreply.github.com>
2022-03-30 10:58:14 +0530

    Add files via upload

---
+++ ndb_1/main.py
@@ -0,0 +1,32 @@
+from google.appengine.ext import ndb
+from google.appengine.ext.webapp import template
+import webapp2
+import jinja2
+import os
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class Book_db(ndb.Model):
+    Book = ndb.StringProperty()
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render({}))
+
+
+class Add_Data(webapp2.RequestHandler):
+    def post(self):
+        new_book=Book_db(Book=self.request.get('book_input'))
+        book_db.put()
+        self.redirect('/')
+
+
+app = webapp2.WSGIApplication([
+    ("/", MainPage),
+    ("/add_data",Add_Data)], debug=True)
